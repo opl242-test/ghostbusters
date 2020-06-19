8495b7519fbc651157bc0d74dbef4780584819bd
opl242-test <67144602+opl242-test@users.noreply.github.com>
2020-06-19 12:00:06 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  const mMenuBtn = $(".m-menu-button");
+  const mMenu = $(".m-menu");
+  const tab = $(".tab");
+
+  mMenuBtn.on("click", function () {
+    console.log("press");
+    mMenu.toggleClass("active");
+    $("body").toggleClass("no-scroll");
+  });
+
+  tab.on("click", function () {
+    tab.removeClass("active");
+    $(this).toggleClass("active");
+    let activeTabContent = $(this).attr("data-target");
+    $(".tabs-content").removeClass("visible");
+    $(activeTabContent).toggleClass("visible");
+  });
+
+  //initialize swiper when document ready
+  var mySwiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    // direction: "vertical",
+    loop: true,
+    spaceBetween: 25,
+    slidesPerView: 4,
+    breakpoints: {
+      992: {
+        slidesPerView: 4,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      320: {
+        slidesPerView: 1,
+        slidesOffsetAfter: 50,
+        navigation: {
+          nextEl: ".button-next",
+        },
+      },
+    },
+  });
+});
